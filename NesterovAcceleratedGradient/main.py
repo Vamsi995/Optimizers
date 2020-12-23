ae2a58cfa28ce8b4e0e0dd481aa2b25e978ee5f7
Vamsi995 <saivamsi.ds123@gmail.com>
2020-12-23 21:35:13 +0530

    adding all the gradient descents

---
+++ NesterovAcceleratedGradient/main.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from descent import GradientDescent
+
+
+def main():
+
+    grad = GradientDescent(0.2,0.5)
+    grad.plotContour()
+
+
+if __name__ == "__main__":
+    main()
